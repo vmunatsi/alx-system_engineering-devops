c2fa39504f04674269187e6f591bc8960b6f1b16
vmunatsi <vincentm43@gmail.com>
2023-01-17 04:48:55 -0800

    task 3 fix

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,41 +1,22 @@
-# Set header with puppet
+# Install and config the nginx
+  exec { 'update':
+  command  => 'sudo apt-get update',
+  provider => shell,
+ }
 
 package { 'nginx':
-  provider => 'gem',
+ ensure  => installed,
+ require => Exec['update'],
 }
 
-file { 'holberton school':
-  path    => '/var/www/html/index.nginx-debian.html',
-  content => 'Holberton School',
-}
-
-file_line { 'Set rewrite':
+file_line { 'headercustom':
+  ensure  => present,
   path    => '/etc/nginx/sites-available/default',
-  match   => '^server {$',
-  line    => 'server {\n\trewrite ^\/redirect_me https:\/\/www.google.com\/ permanent;',
-  replace => 'true',
+  after   => ':80 default_server;',
+  line    => "add_header X-Served-By ${hostname};",
+  require => Package['nginx'],
 }
-
-file { 'Display 404-no-found':
-  path    => '/var/www/html/404_error.html',
-  content => 'Ceci n\'est pas une page',
-}
-
-file_line { 'Set 404-no-found':
-  path    => '/etc/nginx/sites-available/default',
-  match   => '^server {$',
-  line    => 'server {\n\terror_page 404 \/404_error.html;\n\tlocation = \/404_error.html {\n\t\troot \/var\/www\/html;\n\t\tinternal;\n\t}',
-  replace => 'true',
-}
-
-file_line { 'Set header':
-  path    => '/etc/nginx/sites-available/default',
-  match   => 'location \/ {',
-  line    => 'location \/ {\n\t\tadd_header X-Served-By \$hostname;\n',
-  replace => 'true',
-}
-
-exec { 'service nginx restart':
-  command => 'service nginx restart',
-  path    => '/usr/bin/',
+service { 'nginx':
+  ensure  => running,
+  require => File_line['headercustom'],
 }
